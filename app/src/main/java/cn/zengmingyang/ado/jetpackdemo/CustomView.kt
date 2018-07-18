2938d3958f4fb32c32c1e399fd8254cba6225976
Simonla <zengmingyang96@gmail.com>
2018-07-19 00:13:41 +0800

    展示在View中使用Lifecycle

--- app/src/main/java/cn/zengmingyang/ado/jetpackdemo/CustomView.kt
+++ app/src/main/java/cn/zengmingyang/ado/jetpackdemo/CustomView.kt
@@ -1,17 +1,34 @@
 package cn.zengmingyang.ado.jetpackdemo
 
 import android.arch.lifecycle.Lifecycle
+import android.arch.lifecycle.LifecycleObserver
 import android.arch.lifecycle.OnLifecycleEvent
 import android.content.Context
 import android.util.AttributeSet
+import android.util.Log
 import android.widget.FrameLayout
 
-class CustomeView @JvmOverloads constructor(
+class CustomFrameLayoutView @JvmOverloads constructor(
         context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
-) : FrameLayout(context, attrs, defStyleAttr) {
+) : FrameLayout(context, attrs, defStyleAttr),LifecycleObserver {
 
+    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+    fun onCreate() {
+        Log.d(this.javaClass.simpleName, "onCreate")
+    }
 
-    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE) fun onCreate(){
-        
+    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    fun onResume() {
+        Log.d(this.javaClass.simpleName, "onResume")
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    fun onDestroy() {
+        Log.d(this.javaClass.simpleName, "onDestroy")
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+    fun onPause() {
+        Log.d(this.javaClass.simpleName, "onPause")
     }
 }
